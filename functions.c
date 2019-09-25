75e916d2841bc87a2fd6d0afe3ae8ef66f92f367
jdarangop <juandiegoap33@gmail.com>
2019-09-25 15:10:42 +0000

    Add functions push, pull for tests

---
+++ functions.c
@@ -0,0 +1,40 @@
+#include "header.h"
+
+/**
+ * add_dnodeint - Add a new node in the beginning of the list.
+ * @head: Head of the list.
+ * @n: Element into the node.
+ * Return: Node added.
+ */
+stack_t *_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+	return (*stack);
+}
+
+/**
+ * print_dlistint - Print all the elements in the list.
+ * @h: head of the list.
+ * Return: Number of nodes.
+ */
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t current = *stack;
+
+	(void)line_number;
+	while (current != NULL)
+	{
+		printf("%d\n", h->n);
+		current = current->next;
+	}
+}
